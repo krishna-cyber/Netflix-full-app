c4354b93172ca355b69d889cb540f9383dcf2366
krishna tiwari <tiwarikrishna54321@gmail.com>
2022-11-26 11:16:10 +0545

    landing page only successfull

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,14 +1,12 @@
 import React from "react";
 import Feature from "../../components/feature/Feature";
 import Navbar from "../../components/Navbar/Navbar";
-import List from "../../components/List/List";
 
 const Home = () => {
   return (
     <>
       <Navbar />
       <Feature />
-      <List />
     </>
   );
 };
